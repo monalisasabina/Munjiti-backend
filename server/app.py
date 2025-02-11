6dbd64e7c80d18361850ff37c2ae7b78122fe94c
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-02-11 17:49:56 +0300

    started working on app

--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
 from flask import Flask,make_response,request,jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_cors import CORS
+from models import db, User, Pastor, Project, Ministry, MinistryProject, Notice, Downloads, ContactMessage, Notification, cipher
+
+# from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from models import db,ContactMessage
 
@@ -13,6 +16,42 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 api = Api(app)
+CORS(app)
+
+
+# HOME PAGE
+class Home(Resource):
+    def get(self):
+        return{
+            "message":"Welcome to the Munjiti Church API",
+            "Api_version":"v1",
+            "available_endpoints":[
+                "/users",
+                "/pastors"
+            ]
+        },200
+api.add_resource(Home,'/')
+
+# USER
+class Users(Resource):
+    
+    def get(self):
+
+        users_list=[]
+
+        for user in User.query.all():
+
+            user_dict = {
+                "id":user.id,
+                "username":user.username,
+                "role":user.role
+            }
+
+            users_list.append(user_dict)
+
+        return make_response(jsonify(users_list),200)
+
+api.add_resource(Users, '/users')       
 
 
 
