b0db3a4f064bbaeafdc75c5f4b29641761086eae
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-02-21 19:41:53 +0300

    login and checksession API

--- server/app.py
+++ server/app.py
@@ -1,10 +1,15 @@
-from flask import Flask,make_response,request,jsonify
+from flask import Flask,make_response,request,jsonify,session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_cors import CORS
 from models import db, User, Pastor, Project, Ministry, MinistryProject, Notice, Downloads, ContactMessage, Notification, cipher
 
-# from flask_jwt_extended import jwt_required, get_jwt_identity
+import os
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), ".gitignore", ".env")
+load_dotenv(dotenv_path, override=True)
+ADMIN_CODE = os.getenv("ADMIN_CODE")
+# print(f"Loaded ADMIN_CODE: {repr(ADMIN_CODE)}")  # Debugging statement
 
 from models import db,ContactMessage
 
@@ -36,6 +41,7 @@ api.add_resource(Home,'/')
 # Authentication process
 # Signing up
 class Signup(Resource):
+
     def post(self):
 
         data = request.get_json()
@@ -44,6 +50,7 @@ class Signup(Resource):
         password = data.get('password')
         email = data.get('email')
         role = data.get('role')
+        code = data.get('code')
 
         if not username or not password or not email or not role:
             return {'error': 'Username, email , password and role required'}, 400
@@ -57,6 +64,11 @@ class Signup(Resource):
         existing_email = User.query.filter_by(email=email).first()
         if existing_email:
             return {'error':'Email; already taken'}, 400
+        
+        # checking code
+        if code != ADMIN_CODE:
+          return {'error': 'Invalid admin code'},403
+        
 
         # creating new user
         new_user = User(
@@ -65,7 +77,7 @@ class Signup(Resource):
             role=role,
         ) 
 
-        new_user.password_hash(password) = password
+        new_user.password_hash = password
 
         db.session.add(new_user)
         db.session.commit()
@@ -83,6 +95,64 @@ class Signup(Resource):
         },201
     
 api.add_resource(Signup, '/signup', endpoint='signup')    
+
+# Logging in
+class Login(Resource):
+
+    def post(self):
+
+        data =  request.get_json()
+
+        if not data:
+            return {'error':'Invalid JSON format'},400
+        
+        username =  data.get('username')
+        password = data.get('password')
+
+        if not username or not password:
+            return {'error': 'Username and password are required'}, 400
+
+        user = User.query.filter(User.username == username).first()
+
+        if not user:
+            return {'error':'User not found'},401
+        
+        if not user.authenticate(password):
+            return {'error':'Invalid credentials'},401
+        
+        # Store user in session
+        session['user_id'] = user.id
+
+        return {
+            'message':'Logged in successfully',
+            'user':{
+                'name':user.username,
+                'email':user.email,
+                'role':user.role
+            }
+        },200
+
+api.add_resource(Login, '/login', endpoint='login')
+
+
+# Staying logged in
+class CheckSession(Resource):
+    
+    def get(self):
+        if 'user_id' in session:
+            user = User.query.get(session['user_id'])
+
+            if user:
+                return {'message':'User authentication','user':{'id':user.id, 'name':user.username}
+                    },200
+            else:
+                session.pop('user_id', None)
+                return{'error':'User not found'},404
+            
+        return{'error':'Session not found'},401
+
+
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
       
 
